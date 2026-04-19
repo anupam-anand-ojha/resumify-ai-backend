b758cbeeed7767076ec27e8b1e97552150a2a08d
Anupam Anand <theanupamojha999@gmail.com>
2026-04-19 18:10:33 +0530

    Fix: resume routes

--- src/routes/resumeRoutes.js
+++ src/routes/resumeRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
+import { generateResume } from '../../controllers/resumeController'
 
 const router = express.Router()
 
-router.post('/generate-resume', )
+router.post('/generate-resume', generateResume );
 
 export default router
