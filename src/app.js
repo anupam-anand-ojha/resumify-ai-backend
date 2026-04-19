164c25d707a88519b7e9bd61cb5db5b4a5e0a39a
Anupam Anand <theanupamojha999@gmail.com>
2026-04-20 02:26:54 +0530

    update routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import resumeRoutes from './routes/resumeRoutes.js'
 
 const app = express()
 
 app.use(express.json());
+app.use('/api', resumeRoutes);
+app.get("/", (req, res) => {
+  res.send("AI Resume Builder Backend Running");
+});
 
 export default app
