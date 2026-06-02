60ca2062c94c29c0ebf8d3ce00853032cc53e009
Anupam Anand <theanupamojha999@gmail.com>
2026-06-02 22:55:00 +0530

    connect frontend

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 import express from 'express'
 import resumeRoutes from './routes/resumeRoutes.js'
+import cors from 'cors'
 
 const app = express()
+app.use(cors({
+  origin:[
+   'http://localhost:5173/',
+   'getresumify.vercel.app'
+  ],
+  credentials: true
+}))
+
 
 app.use(express.json());
 app.use('/api', resumeRoutes);
